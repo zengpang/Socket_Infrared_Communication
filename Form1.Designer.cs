26103571057b185be440aadc0f4b17db9f2fcc4f
胖 曾 <2516629927@qq.com>
2020-09-15 15:52:24 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Socket红外通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Open_Btn = new System.Windows.Forms.Button();
+            this.Close_Btn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SendTimer = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Open_Btn
+            // 
+            this.Open_Btn.Location = new System.Drawing.Point(55, 83);
+            this.Open_Btn.Name = "Open_Btn";
+            this.Open_Btn.Size = new System.Drawing.Size(232, 45);
+            this.Open_Btn.TabIndex = 0;
+            this.Open_Btn.Text = "打开串口";
+            this.Open_Btn.UseVisualStyleBackColor = true;
+            this.Open_Btn.Click += new System.EventHandler(this.Open_Btn_Click);
+            // 
+            // Close_Btn
+            // 
+            this.Close_Btn.Location = new System.Drawing.Point(55, 134);
+            this.Close_Btn.Name = "Close_Btn";
+            this.Close_Btn.Size = new System.Drawing.Size(232, 45);
+            this.Close_Btn.TabIndex = 1;
+            this.Close_Btn.Text = "关闭串口";
+            this.Close_Btn.UseVisualStyleBackColor = true;
+            this.Close_Btn.Click += new System.EventHandler(this.Close_Btn_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // SendTimer
+            // 
+            this.SendTimer.Tick += new System.EventHandler(this.SendTimer_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(124, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 233);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Close_Btn);
+            this.Controls.Add(this.Open_Btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open_Btn;
+        private System.Windows.Forms.Button Close_Btn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer SendTimer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket红外通信
+{
+    public partial class Form1 : Form
+    {
+        private string SendProtocol= "ee e1 01 55 ff fc fd ff";
+        private byte[] SendByte = new byte[8];
+        private byte[] InputDate = new byte[4000];
+        private Socket WatherSocket;
+        private Socket SendSocket;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Close_Btn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            SendTimer.Stop();
+        }
+
+        private void Open_Btn_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text.Trim();
+            serialPort1.BaudRate =Convert.ToInt32(textBox1.Text.Trim());
+           
+            serialPort1.Open();
+            SendTimer.Start();
+        
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("192.168.5.101"), 40000);
+            WatherSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            try
+            {
+                WatherSocket.Bind(iPEnd);
+                WatherSocket.Listen(12);
+            }
+            catch
+            {
+                MessageBox.Show("连接发生错误");
+            }
+            Thread th_wait = new Thread(WaitClient);
+            th_wait.IsBackground = true;
+            th_wait.Start();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+              
+        Thread.Sleep(300);
+            serialPort1.Read(InputDate, 0, (4000>serialPort1.BytesToRead? serialPort1.BytesToRead:4000));
+            SendMessage();
+            label1.Text= (InputDate[11] * 256 + InputDate[12]).ToString();
+           
+        }
+
+        private void SendTimer_Tick(object sender, EventArgs e)
+        {
+            SendTimer.Stop();
+           
+            try
+            {
+            
+                string[] SendMessageContent = SendProtocol.Split(' ');
+                for (int i = 0; i < 8; i++)
+                {
+                    SendByte[i] = Convert.ToByte(SendMessageContent[i], 16);
+                }
+                serialPort1.Write(SendByte, 0, SendByte.Length);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("发送错误");
+            }
+            SendTimer.Start();
+
+
+
+        }
+        private void SendMessage()//发送消息
+        {
+         
+            try
+            {
+       
+                SendSocket.Send(InputDate);
+            }
+            catch
+            {
+                MessageBox.Show("发送失败");
+            }
+        }
+        private void WaitClient()
+        {
+            while(true)
+            {
+                SendSocket = WatherSocket.Accept();
+            }
+       
+
+        }
+      
+    }
+}
